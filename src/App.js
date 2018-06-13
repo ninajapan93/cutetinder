ba0a4b90a8c0a0d30ddc11fd8d3a007c1c78a2fc
Unknown <aleidacontact@gmail.com>
2018-06-13 16:47:36 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Japón</h1>
+        <p>ahsdj akshdl a ajkhdjk ai kashd <br></br>
+          sajh dajs hs ahj</p> 
+          <img src="http://e01-expansion.uecdn.es/assets/multimedia/imagenes/2017/09/29/15067006519917.jpg"></img>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
