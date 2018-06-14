aaf144f89ba93d98db0a445de9c8f9351d838122
Unknown <aleidacontact@gmail.com>
2018-06-14 10:29:47 +0200

    Todo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './include/bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Japón</h1>
-        <p>ahsdj akshdl a ajkhdjk ai kashd <br></br>
-          sajh dajs hs ahj</p> 
-          <img src="http://e01-expansion.uecdn.es/assets/multimedia/imagenes/2017/09/29/15067006519917.jpg"></img>
-        
+        <div className="header">
+          <h1>Porque odio los despertadores<strike></strike></h1>
+          <h4>ESPECIAL MENTE EL TUYO</h4>
+        </div>
+        <p>Vale en esta columna explicare el porque me puede afectar tanto/odiarlo aunque te parezca una tonteria.</p>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <h4>Contras</h4>
+              <ul>
+                <li>La música es horrible</li>
+                <li>No despierta cabrea</li>
+                <li>Me has creado un trauma con tanto aplazamiento</li>
+              </ul>
+            </div>
+            <div className="col">
+              <h4>Pros</h4>
+            <ul>
+              <li>Reconozco que me despierta pero no de la manera que quiero</li>
+              <li>Si un dia te aburres y quieres crear pelea la alarma funciona</li>
+            </ul>
+            </div>
+          </div>     
+        </div>
+        <div><p>En definitiva bebe o cambias la canço de la alarma o posa el meu movilus porfa</p></div>
       </div>
+        
+    
     );
   }
 }
